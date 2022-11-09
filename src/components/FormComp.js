76eb3658ce83819369eba857b172e010258384e6
vih0 <araujovitoris1.4@gmail.com>
2022-11-09 17:18:08 -0300

    api fake ok

--- src/components/FormComp.js
+++ src/components/FormComp.js
@@ -1,45 +1,54 @@
 import React from "react";
 import { useState } from "react";
+import "./index.css";
+import axios from "axios";
+
 
 function FormComp() {
-    const [values, SetValues] = useState();
+  const [Name, SetName] = useState("");
+  const [Book, SetBook] = useState("");
+  const [Category, SetCategory] = useState("");
+  const PostData = () => {
     
-    const handleChangeValues = (value) => {
-        SetValues((prevValue) => ({
-            ...prevValue,
-            [value.target.name]: value.target.value,
-        }))
-    }
-    const AddValues = () => {
-        console.log(values)
-    }
-    return (
-        <div className="App">
-            <div className="container-wrapp">
-                <h1 className="title">Lista de Livros</h1>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Autor"
-                    className="input"
-                    onChange={handleChangeValues}></input>
-                <input
-                    type="text"
-                    name="book"
-                    placeholder="Nome do Livro"
-                    className="input"
-                    onChange={handleChangeValues}>
+      axios.post(`https://636bda197f47ef51e13c1fe5.mockapi.io/api/v1/books`, {
+          Name,
+          Book,
+          Category
+      })
+  }
+   
 
-                </input>
-                <input
-                    type="text"
-                    name="category"
-                    placeholder="Categoria"
-                    className="input"
-                    onChange={handleChangeValues}></input>
-                <button className="btn-Add" onClick={AddValues}>Adicionar!</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div className="container-wrapp">
+        <h1 className="title">Lista de Livros</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Autor"
+          className="input"
+          onChange={(e) => SetName(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          name="book"
+          placeholder="Nome do Livro"
+          className="input"
+          onChange={(e) => SetBook(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          name="category"
+          placeholder="Categoria"
+          className="input"
+          onChange={(e) => SetCategory(e.target.value)}
+        ></input>
+        <button className="btn-Add" onClick={PostData}>
+          Adicionar!
+        </button>
+      </div>
+    </div>
+  );
 }
+
 export default FormComp;
